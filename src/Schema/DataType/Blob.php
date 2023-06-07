d8088add3c9d0cfb75b8c392d98a0b34bebec568
Brad Harrison <brad@communityds.com.au>
2023-06-07 13:04:54 +0930

    Updated blob data type to support arrays

--- src/Schema/DataType/Blob.php
+++ src/Schema/DataType/Blob.php
@@ -3,12 +3,17 @@
 namespace CommunityDS\Deputy\Api\Schema\DataType;
 
 /**
- * Values are string within API and PHP land.
+ * Values are mixed within PHP land but string when sent to the API.
  */
 class Blob extends VarChar
 {
+    public function fromApi($value)
+    {
+        return $value;
+    }
+
     public function phpType()
     {
-        return 'string';
+        return 'mixed';
     }
 }
